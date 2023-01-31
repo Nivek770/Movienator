d69bfec344126b061e8ae8a3ceefb1f5be22793c
Kevin Hasse <kevin.gideon.hasse@gmail.com>
2023-01-31 16:16:28 +0100

    unhandled Rejection logging

--- movienator_react/src/App.tsx
+++ movienator_react/src/App.tsx
@@ -5,6 +5,10 @@ import router from './Router';
 /*
 Creating the Router Provider from the exportet Route
  */
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+});
+
 function App() {
   return <RouterProvider router={router} />;
 }
